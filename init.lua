d1dbbcbcb74febb9373cc2aea7eb5888d6994c50
Montandalar <jbis1337@hotmail.com>
2021-02-03 22:56:27 +1100

    Add cucumbers and three colours of capsicum

--- init.lua
+++ init.lua
@@ -1,14 +1,21 @@
 local S = minetest.get_translator("foodblocks")
 
-local function regfoodblock(name, desc, ingredient)
+--[[ regfoodblock
+Register a whole cube food block.
+By default it will have have a 1 wide x 3 high texture sheet.
+In vertical order: top, then sides, then bottom.
+ name: the internal name for the block and prefix for its texture *_cube.png
+ desc: the displayed name for the block, which will be localised
+ ingredient: the food used to craft the block and that it drops
+ customtiles: use the provided tiles for a texture instead of the sheet
+ --]]
+local function regfoodblock(name, desc, ingredient, customtiles)
 	local tile_sides = name..'_cube.png^[sheet:1x3:0,1'
 	local node_name = "foodblocks:"..name.."_cube"
-	minetest.register_node(node_name, {
-		description = S("@1 Block", S(desc)),
-		drop = string.format('"%s" 9', ingredient),
-		groups = {choppy = 3, oddly_breakable_by_hand = 2},
-		drawtype = 'normal',
-		paramtype2 = "facedir",
+	local tiles
+	if customtiles then
+		tiles = customtiles
+	else
 		tiles = {
 			name..'_cube.png^[sheet:1x3:0,0', -- top
 			name..'_cube.png^[sheet:1x3:0,2', -- bottom
@@ -17,6 +24,14 @@ local function regfoodblock(name, desc, ingredient)
 			tile_sides,
 			tile_sides,
 		}
+	end
+	minetest.register_node(node_name, {
+		description = S("@1 Block", S(desc)),
+		drop = string.format('"%s" 9', ingredient),
+		groups = {choppy = 3, oddly_breakable_by_hand = 2},
+		drawtype = 'normal',
+		paramtype2 = "facedir",
+		tiles = tiles,
 	})
 	
 	minetest.register_craft({
@@ -45,6 +60,19 @@ if farming.mod then
 	end
 end
 
+local function reg_capsicum(colourname, desc, ingredient, multi_color)
+	local s_multi = "^[multiply:"..multi_color
+	local tile_sides = "foodblocks_capsicum_base.png^[sheet:1x3:0,1" .. s_multi
+	regfoodblock("capsicum_"..colourname, desc.." Capsicum", ingredient, {
+		-- top
+		"foodblocks_capsicum_base.png^[sheet:1x3:0,0"..s_multi.."^foodblocks_capsicum_stalk.png",
+		-- bottom 
+		"foodblocks_capsicum_base.png^[sheet:1x3:0,2"..s_multi,
+		-- sides 
+		tile_sides, tile_sides, tile_sides, tile_sides
+	})
+end
+
 -- Redo + Undo
 if ffork >= 1 then
 	regfoodblock("beetroot", "Beetroot", "farming:beetroot")
@@ -53,13 +81,15 @@ if ffork >= 1 then
 	regfoodblock("cabbage", "Cabbage", "farming:cabbage")
 	--TODO: Carrot
 	regfoodblock("corn", "Corn", "farming:corn")
-	--TODO: Cucumber
+	regfoodblock("cucumber", "Cucumber", "farming:cucumber")
 	--TODO: Garlic
 	--TODO: Lettuce
 	regfoodblock("onion", "Onion", "farming:onion")
-	--TODO: Capsicum (x3?)
+	reg_capsicum("g", "Green", "farming:pepper", "#87a644")
+	reg_capsicum("y", "Yellouw", "farming:pepper_y", "#ffdc17")
+	reg_capsicum("r", "Red", "farming:pepper_r", "#f83f3f")
 	regfoodblock("pineapple", "Pineapple", "farming:pineapple")
-	regfoodblock("potato", "Potato", "farming:potato")
+	regfoodblock("potato", "Potato", "farming:potato", {"potato_cube.png"})
 	--TODO: Raspberry
 	regfoodblock("tomato", "Tomato", "farming:tomato")
 end
